cf6fbeb8cc338e1d021dd1675bd5419bf4bea57f
Martti <martti@aukia.com>
2022-02-02 15:29:35 +0200

    add searching for insecure cookies in headers
    search all headers for set-cookies
    that don't also use httpOnly and secure
    
    matches won't all be critical but
    allows user to manually verify if
    that info should be visible
    
    fix not finding any set-cookie headers
    
    add table for insecure cookies

--- src/pages/Content/modules/warnings.ts
+++ src/pages/Content/modules/warnings.ts
@@ -1,39 +1,13 @@
-import type { FunctionComponent } from 'react'
 import StorageReader from './storageReader'
-import type { JWTMessage } from '../../Background/jwt'
-import type { IStorageReader } from './storageReader'
 import JWTTable from './readers/jwt_reader/JWTTable'
+import InsecureCookieTable from './readers/insecure_cookie/insecureCookie'
 import JWTFailLog from './readers/failed_jwt_reader/failLog'
 
+import type { FunctionComponent } from 'react'
+import type { JWTMessage } from '../../Background/jwt'
+import type { IStorageReader } from './storageReader'
+import type { InsecureCookieHeader } from '../../Background/insecureCookies'
 import type { storageKey } from '../../Background/utilities'
-// import { JsonValue } from 'type-fest'
-// type warning<U extends JsonValue> = {
-//   [K in U]: warning<K>
-// }[U]
-// type x = warning<JsonValue>
-// type warning<T> = T extends JsonValue ? warning<T> : never
-// type y = warning<JsonValue>
-
-// type Generic<T> = { something: T }
-// type GenericOfUnion<U extends string> = { [K in U]: Generic<K> }[U]
-// type Foo = GenericOfUnion<NonNullable<JsonValue>>
-
-// type warning<T extends JsonValue> = {
-// reader: IStorageReader<T>
-// presenter: FunctionComponent<{ data: T[] }>
-// }
-// type DistributeWarning<U> = U extends JsonValue ? warning<U> : never
-// export type WarningHandler = DistributeWarning<JsonValue | JWTMessage>
-
-// const warnings = (): { [key: string]: WarningHandler } => {
-// TODO: clean this mess
-// const jwts: warning<{ [key: string]: JsonValue }> = {
-//   reader: StorageReader<{ [key: string]: JsonValue }>('jwt'),
-//   presenter: JWTTable as FunctionComponent<{
-//     data: { [key: string]: JsonValue }[]
-//     clear: () => void
-//   }>,
-// }
 
 type Handler<T> = {
   reader: IStorageReader<T>
@@ -44,9 +18,13 @@ type jwtHandler = Handler<JWTMessage>
 
 type jwtFailHandler = Handler<string>
 
-// Includes<jwtFailHandler.presenter IStorageReader<string>>
+type insecureCookieHandler = Handler<object>
 
-export type IWarningHandlers = { JWTs: jwtHandler; 'False+': jwtFailHandler }
+export type IWarningHandlers = {
+  JWTs: jwtHandler
+  'False+': jwtFailHandler
+  'vulnerable cookie': insecureCookieHandler
+}
 
 const warningHandlers = (): IWarningHandlers => {
   const handlerFactory = <T>(
@@ -60,6 +38,10 @@ const warningHandlers = (): IWarningHandlers => {
   return {
     JWTs: handlerFactory<JWTMessage>('jwt', JWTTable),
     'False+': handlerFactory<string>('jwt-fail', JWTFailLog),
+    'vulnerable cookie': handlerFactory<InsecureCookieHeader>(
+      'insecure-cookie',
+      InsecureCookieTable
+    ),
   }
 }
 
