46220335b17fa7715d6e057313c6c72348baa5f3
Martti <martti@aukia.com>
2022-02-04 14:56:01 +0200

    fix insecure cookies detection
    literally only found cases where
    the headers were present

--- src/pages/Background/insecureCookies.ts
+++ src/pages/Background/insecureCookies.ts
@@ -3,10 +3,10 @@ import { save } from './utilities'
 export const findInsecureCookies = (headerValue: string): SecurityTags[] => {
   const res: SecurityTags[] = []
   const normalizedValue = headerValue.toLowerCase() // case insensitive as per: https://datatracker.ietf.org/doc/html/rfc6265#section-5.2.6
-  if (normalizedValue.includes('httponly')) {
+  if (!normalizedValue.includes('httponly')) {
     res.push('HttpOnly')
   }
-  if (normalizedValue.includes('secure')) {
+  if (!normalizedValue.includes('secure')) {
     res.push('Secure')
   }
   return res
