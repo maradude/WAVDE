bcab8dca6731ceb6afceec2df04992eae364be3a
Martti <martti@aukia.com>
2022-02-23 11:42:52 +0200

    attempt to fix service worker going to sleep
    Alarm should wake up service worker
    every minute. So far it doesn't seem to
    be enough as service worker sometimes
    goes to sleep in less than a minute

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,6 +1,20 @@
 import { onHeadersReceivedHandler, onSendHeadersHandler } from './headerHandler'
 import onBeforeRequestHandler from './requestHandler'
 
+chrome.alarms.create({ periodInMinutes: 1 })
+
+chrome.alarms.onAlarm.addListener((params) => {
+  console.log(params)
+  let text: string
+  try {
+    const dt = new Date(params.scheduledTime) // format example: 1645462419031.02
+    text = `${dt.getHours()}${dt.getMinutes()}`
+  } catch {
+    text = 'err'
+    console.error('issue with getting badge time: ', params)
+  }
+  chrome.action.setBadgeText({ text })
+})
 const networkFilters = {
   urls: ['<all_urls>'],
 }
